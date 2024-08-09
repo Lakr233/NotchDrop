6bd8b9d525935c14d512b2fdd0b91cf4a2c12401
砍砍 <git@qaq.wiki>
2024-08-09 13:05:59 +0800

    Update 2.7.22.AAPL

--- NotchDrop/NotchView.swift
+++ NotchDrop/NotchView.swift
@@ -66,7 +66,7 @@ struct NotchView: View {
                 ).animation(vm.animation)
             )
         }
-        .background(dragDetecter)
+        .background(dragDetector)
         .animation(vm.animation, value: vm.status)
         .preferredColorScheme(.dark)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
@@ -136,18 +136,25 @@ struct NotchView: View {
     }
 
     @ViewBuilder
-    var dragDetecter: some View {
-        if vm.status == .closed {
-            RoundedRectangle(cornerRadius: notchCornerRadius)
-                .foregroundStyle(Color.black.opacity(0.001)) // fuck you apple and 0.001 is the smallest we can have
-                .contentShape(Rectangle())
-                .frame(width: vm.deviceNotchRect.width + vm.dropDetectorRange, height: vm.deviceNotchRect.height + vm.dropDetectorRange)
-                .onDrop(of: [.data], isTargeted: $dropTargeting) { _ in true }
-                .onChange(of: dropTargeting) { newValue in if newValue {
+    var dragDetector: some View {
+        RoundedRectangle(cornerRadius: notchCornerRadius)
+            .foregroundStyle(Color.black.opacity(0.001)) // fuck you apple and 0.001 is the smallest we can have
+            .contentShape(Rectangle())
+            .frame(width: notchSize.width + vm.dropDetectorRange, height: notchSize.height + vm.dropDetectorRange)
+            .onDrop(of: [.data], isTargeted: $dropTargeting) { _ in true }
+            .onChange(of: dropTargeting) { isTargeted in
+                if isTargeted, vm.status == .closed {
+                    // Open the notch when a file is dragged over it
                     vm.notchOpen(.drag)
                     vm.hapticSender.send()
-                } }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        }
+                } else if !isTargeted {
+                    // Close the notch when the dragged item leaves the area
+                    let mouseLocation: NSPoint = NSEvent.mouseLocation
+                    if !vm.notchOpenedRect.insetBy(dx: vm.inset, dy: vm.inset).contains(mouseLocation) {
+                        vm.notchClose()
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
