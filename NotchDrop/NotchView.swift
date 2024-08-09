e00779f8e8203f457c045b824e9312b7612a00da
砍砍 <git@qaq.wiki>
2024-08-09 13:03:47 +0800

    Bump Version

--- NotchDrop/NotchView.swift
+++ NotchDrop/NotchView.swift
@@ -138,19 +138,19 @@ struct NotchView: View {
     @ViewBuilder
     var dragDetector: some View {
         RoundedRectangle(cornerRadius: notchCornerRadius)
-            .foregroundStyle(Color.black.opacity(0.001))  // fuck you apple and 0.001 is the smallest we can have
+            .foregroundStyle(Color.black.opacity(0.001)) // fuck you apple and 0.001 is the smallest we can have
             .contentShape(Rectangle())
             .frame(width: notchSize.width + vm.dropDetectorRange, height: notchSize.height + vm.dropDetectorRange)
             .onDrop(of: [.data], isTargeted: $dropTargeting) { _ in true }
             .onChange(of: dropTargeting) { isTargeted in
-                if isTargeted && vm.status == .closed{
+                if isTargeted, vm.status == .closed {
                     // Open the notch when a file is dragged over it
                     vm.notchOpen(.drag)
                     vm.hapticSender.send()
-                } else if !isTargeted{
+                } else if !isTargeted {
                     // Close the notch when the dragged item leaves the area
                     let mouseLocation: NSPoint = NSEvent.mouseLocation
-                    if  !vm.notchOpenedRect.insetBy(dx: vm.inset, dy: vm.inset).contains(mouseLocation){
+                    if !vm.notchOpenedRect.insetBy(dx: vm.inset, dy: vm.inset).contains(mouseLocation) {
                         vm.notchClose()
                     }
                 }
